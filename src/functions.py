02009e126ffe3bd4339cc0cf604dc7bba045f004
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-08-10 23:17:23 -0300

    fix(function): ajusta chamada de string.punctuation + remove download dos pacotes na função

--- src/functions.py
+++ src/functions.py
@@ -1,20 +1,14 @@
-import nltk
 from nltk.corpus import stopwords
 import string
 
 # usar com .apply()
 def remove_stopwords_punctuation(text):
-    # adiciona pacotes de stopwords e pontuação
-    nltk.download("stopwords")
-    nltk.download("punkt")
-    nltk.download("punkt_tab")
-
     # remove stopwords
     stops = stopwords.words("portuguese")
     no_stopwords = [p for p in text if p not in stops]
 
     # remove pontuação
-    puncts = string.punctuation()
+    puncts = string.punctuation
     no_punctuation = [p for p in no_stopwords if p not in puncts]
 
     return no_punctuation
